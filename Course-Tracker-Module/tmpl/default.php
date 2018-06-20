03cdbefdba74ba710b6adc10b3576fa9bb7a114d
link375 <github.com/link375>
2018-06-20 12:42:56 -0600

    added css styling

--- Course-Tracker-Module/tmpl/default.php
+++ Course-Tracker-Module/tmpl/default.php
@@ -36,8 +36,96 @@ defined('_JEXEC') or die;
 }
 
 .progressBar {
+
+        /* Reset the default appearance */
+        -webkit-appearance: none;
+        -moz-appearance: none;
+        appearance: none;
+
         width: 100%;
+        height: 20px;
         margin: 5px;
+
+        /* Firefox */
+        border: none;
+        background: #EEE;
+        border-radius: 3px;
+        box-shadow: 0 2px 3px rgba(0,0,0,0.2) inset;
+
+}
+
+.progressBar progress::-webkit-progress-bar {
+        background: #EEE;
+        box-shadow: 0 2px 3px rgba(0,0,0,0.2) inset;
+        border-radius: 3px;
+}
+
+.progressBar progress::-webkit-progress-value {
+        background-color: #EEE;
+        border-radius: 3px;
+}
+
+.progressBar progress::-moz-progress-bar {
+        background-color: #EEE;
+        border-radius: 3px;
+}
+
+#ajaxButton {
+        display: inline-block;
+        border-radius: 4px;
+        background-color: #f4511e;
+        border: none;
+        color: #FFFFFF;
+        text-align: center;
+        font-size: 16px;
+        padding: 10px;
+        width: 100%;
+        transition: all 0.5s;
+        cursor: pointer;
+        margin: 5px;
+}
+
+#ajaxButton span {
+        cursor: pointer;
+        display: inline-block;
+        position: relative;
+        transition: 0.5s;
+}
+
+#ajaxButton span:after {
+        content: '\00bb';
+        position: absolute;
+        opacity: 0;
+        top: 0;
+        right: -5px;
+        transition: 0.5s;
+}
+
+#ajaxButton:hover span {
+        padding-right: 5px;
+}
+
+#ajaxButton:hover span:after {
+        opacity: 1;
+        right: 0;
+}
+
+.total {
+        font-weight: bold;
+        width: 100%;
+        text-align: center;
+}
+
+.current {
+        font-weight: bold;
+        width: 100%;
+        text-align: center;
+}
+
+.percent {
+        font-weight: bold;
+        width: 100%;
+        text-align: center;
 }
 
 </style>
